75991ae12f922f98b5a4236978d1557bf2a88a55
CbtGit73 <145291812+CbtGit73@users.noreply.github.com>
2024-01-20 18:41:35 -0600

    Update 02-S3Buckets.tf

--- 02-S3Buckets.tf
+++ 02-S3Buckets.tf
@@ -10,6 +10,44 @@ resource "aws_s3_bucket" "buckets" {
 
     bucket = "${var.bucket_name}" 
 
-    acl = "${var.acl_value}"   
+    acl = "${var.acl_value}"
 
+
+#or
+
+
+resource "aws_s3_bucket" "S3-bucket1" {
+  bucket = "CloudNinjaInspirationPics&Gifs"
+
+  tags = {
+    Name        = "CloudNinjaInspirationPics&Gifs"
+    Environment = "Dev"
+  }
+}
+
+
+resource "aws_s3_bucket_ownership_controls" "object-ownership" {
+  bucket = aws_s3_bucket.CloudNinjaInspirationPics&Gifs.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public-access-settings" {
+  bucket = aws_s3_bucket.CloudNinjaInspirationPics&Gifs.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "acl-settings" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.CloudNinjaInspirationPics&Gifs,
+    aws_s3_bucket_public_access_block.CloudNinjaInspirationPics&Gifs,
+  ]
+
+  bucket = aws_s3_bucket.CloudNinjaInspirationPics&Gifs.id
+  acl    = "public-read"
 }
