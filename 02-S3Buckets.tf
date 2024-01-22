1cc649a0500d071f99169d5f416b528c0c140563
CbtGit73 <145291812+CbtGit73@users.noreply.github.com>
2024-01-21 18:41:08 -0600

    Update 02-S3Buckets.tf

--- 02-S3Buckets.tf
+++ 02-S3Buckets.tf
@@ -51,3 +51,23 @@ resource "aws_s3_bucket_acl" "acl-settings" {
   bucket = aws_s3_bucket.CloudNinjaInspirationPics&Gifs.id
   acl    = "public-read"
 }
+
+#Policy to make picture show
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.bucket.id
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Sid" : "PublicReadGetObject",
+          "Effect" : "Allow",
+          "Principal" : "*",
+          "Action" : "s3:GetObject",
+          "Resource" : "arn:aws:s3:::${aws_s3_bucket.bucket.id}/*"
+        }
+      ]
+    }
+  )
+}
