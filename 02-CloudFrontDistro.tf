b5938a1fc6e60dd457d189e91ae80b56f6a0e354
CbtGit73 <145291812+CbtGit73@users.noreply.github.com>
2024-02-03 13:42:57 -0600

    Update 02-CloudFrontDistro.tf

--- 02-CloudFrontDistro.tf
+++ 02-CloudFrontDistro.tf
@@ -29,6 +29,8 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
       }
     }
 
+    price_class = "PriceClass_100"
+
     viewer_protocol_policy = "allow-all"
     min_ttl                = 0
     default_ttl            = 3600
