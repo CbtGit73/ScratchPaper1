d33661bf5fd418ead6846495e9c5d88d4eacb34a
CbtGit73 <145291812+CbtGit73@users.noreply.github.com>
2024-02-05 19:10:19 -0600

    Update 01-S3Buckets.tf

--- 01-S3Buckets.tf
+++ 01-S3Buckets.tf
@@ -72,20 +72,3 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
   )
 }
 */
-
-/*
-# Variable s# Bucket******
-module "s3" {
-
-    source = "</Users/christopher/Cloud_Ninjas_Pics_Gifs/Pics_Gifs>"
-
-    bucket_name = "Pics_Gifs"       
-
-}
-
-resource "aws_s3_bucket" "buckets" {
-
-    bucket = "${var.bucket_name}" 
-
-    acl = "${var.acl_value}"
-*/
