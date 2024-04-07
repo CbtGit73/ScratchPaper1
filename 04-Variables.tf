ede4896a3e3022be789e40cbaac798e2da2a9ea4
CbtGit73 <145291812+CbtGit73@users.noreply.github.com>
2024-04-07 14:29:47 -0500

    Update 04-Variables.tf

--- 04-Variables.tf
+++ 04-Variables.tf
@@ -2,21 +2,22 @@ variable "objects" {
   type = map(string)
 }
 
+# Create a VPC
+aws ec2 create-vpc --cidr-block 10.0.0.0/16 --output text
 
+# Add a tag to the VPC
+aws ec2 create-tags --resources vpc-0f3feedb2b7452c52 --tags Key=Name,Value=CBTVPC --output text
 
-resource "aws_dynamodb_table_item" "upload" {
-  table_name = aws_dynamodb_table.dynamodb_table.name
-  hash_key   = aws_dynamodb_table.dynamodb_table.hash_key
-  item       = <<EOF
- {
-  "Artist": {"S": "Miles_Davis"},
-  "AlbumTitle": {"S": "Kind_Of_Blue"},
-  "Year": {"N": "1959"},
-  "Pressing": {"S": "QRP"}
-  "RepressYear": {"N": "2022"},
-  "MediaCondition": {"S": "Mint"}
-  "SleeveCondition": {"S": "Mint"},
-  "MedianValue": {"N": "130"}  
-}
-EOF
-}
+aws ec2 modify-vpc-attribute --vpc-id vpc-0f3feedb2b7452c52 --enable-dns-hostnames '{"Value":true}'
+
+aws ec2 create-subnet --vpc-id vpc-0f3feedb2b7452c52 --cidr-block 10.0.1.0/24 --output text
+
+aws ec2 create-tags --resources subnet-0478960f44954c740   --tags Key=Name,Value=MyPublicSubnet --output text
+
+# Create an Internet Gateway
+aws ec2 create-internet-gateway --output text
+
+# Add a tag to the Internet Gateway
+aws ec2 create-tags --resources igw-089922953fb7ae8b7  --tags Key=Name,Value=MyInternetGateway --output text
+
+aws ec2 attach-internet-gateway --internet-gateway-id igw-089922953fb7ae8b7 --vpc-id vpc-0f3feedb2b7452c52 --output text
